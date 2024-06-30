57f0b12aa121544e81b6942d1287cfe62159ec91
vtonu <victor.tonu@yahoo.com>
2024-06-30 14:52:49 -0700

    Updated text

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -14,9 +14,9 @@ const FeatureSection = () => {
           <div key={index} className="w-full border rounded-md sm:w-1/2 lg:w-1/4">
             <div className="grid gap-4 justify-items-center">
               <div className="pt-8 hover:cursor-default">{feature.text}</div>
-              {/* <div className="flex items-center justify-center w-10 h-10 text-blue-700 rounded-md bg-neutral-100 hover:bg-neutral-200 hover:cursor-pointer">
+              <div className="flex items-center justify-center w-10 h-10 text-blue-700 rounded-md bg-neutral-100 hover:bg-neutral-200 hover:cursor-pointer">
                 {feature.icon}
-              </div> */}
+              </div>
               <p className="pb-2 mb-2 text-md text-amber-50">{feature.description}</p>
             </div>
           </div>
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -40,21 +40,6 @@ export const features = [
       </div>
     ),
   },
-
-  {
-    icon: <Copy />,
-    text: (
-      <span className="px-4 py-2 text-sm font-medium tracking-wide text-blue-600 uppercase rounded-sm bg-neutral-100">
-        Business Type
-      </span>
-    ),
-    description: (
-      <div className="p-2 font-normal border rounded-md bg-neutral-800 text-md border-neutral-600">
-        <p className="text-center ">LLC</p>
-      </div>
-    ),
-  },
-
   {
     icon: <Copy />,
     text: (
@@ -64,7 +49,7 @@ export const features = [
     ),
     description: (
       <div className="p-2 font-normal border rounded-md bg-neutral-800 text-md border-neutral-600">
-        <p className="text-center ">December 21, 2023 (WA SOS Certificate)</p>
+        <p className="text-center ">December 21, 2023 (WASOS Certificate)</p>
       </div>
     ),
   },
@@ -79,10 +64,6 @@ export const features = [
       <div className="p-2 font-normal border rounded-md bg-neutral-800 text-md border-neutral-600">
         <p className="text-center ">6053833120010001 - RESIDENT - ACTIVE</p>
         <p className="text-center ">Expires: Dec 31, 2024</p>
-        <br></br>
-        <p className="text-center ">INDUSTRIAL INSURANCE - ACTIVE</p>
-        <p className="text-center ">UNEMPLOYMENT INSURANCE - ACTIVE</p>
-        <p className="text-center ">TAX REGISTRATION - ACTIVE</p>
       </div>
     ),
   },
@@ -96,7 +77,6 @@ export const features = [
     description: (
       <div className="p-2 font-normal border rounded-md bg-neutral-800 text-md border-neutral-600">
         <p className="text-center ">605383312 - NON-RESIDENT - ACTIVE</p>
-        <p className="text-center ">Expires: ?</p>
       </div>
     ),
   },
@@ -152,7 +132,6 @@ export const features = [
     description: (
       <div className="p-2 font-normal border rounded-md bg-neutral-800 text-md border-neutral-600">
         <p className="text-center ">METEREL772R7</p>
-        <p className="text-center ">Effective: 01/08/2024</p>
         <p className="text-center ">Expires: 01/08/2026</p>
       </div>
     ),
@@ -167,7 +146,6 @@ export const features = [
     description: (
       <div className="p-2 font-normal border rounded-md bg-neutral-800 text-md border-neutral-600">
         <p className="text-center ">Permit Number: A57940926</p>
-        <p className="text-center ">Effective Date: Feb-16-2024</p>
         <p className="text-center ">Expiration Date: Feb-15-2026</p>
       </div>
     ),
