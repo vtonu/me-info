c17d05cd42b66952a1aab2ac4f8919f1c2db89f2
vtonu <victor.tonu@yahoo.com>
2024-06-30 15:47:51 -0700

    Copy to clipboard!

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -4,10 +4,17 @@ import { features } from '../constants';
 const FeatureSection = () => {
   const [clickedStatus, setClickedStatus] = useState({});
 
-  const handleClick = (index) => {
-    setClickedStatus({ ...clickedStatus, [index]: true });
+  const copyText = (text) => {
+    navigator.clipboard.writeText(text).then(
+      () => console.log('Copied to clipboard successfully!'),
+      (err) => console.error('Failed to copy to clipboard', err),
+    );
+  };
 
-    // Change text to "Copied!" and revert back to "COPY" after 1.5 seconds
+  const handleClick = (index, textToCopy) => {
+    setClickedStatus({ ...clickedStatus, [index]: true });
+    copyText(textToCopy);
+
     setTimeout(() => {
       setClickedStatus((prevState) => ({ ...prevState, [index]: false }));
     }, 1500);
@@ -31,9 +38,9 @@ const FeatureSection = () => {
                 className={`flex items-center justify-center w-auto h-10 text-blue-700 rounded-md bg-neutral-100 hover:bg-neutral-200 px-2 ${
                   clickedStatus[index] ? 'bg-neutral-300 border-2 text-blue-600' : ''
                 }`}
-                onClick={() => handleClick(index)}>
+                onClick={() => handleClick(index, feature.copyText)}>
                 {feature.icon}
-                <span className="ml-2">{clickedStatus[index] ? 'Copied!' : feature.copyText}</span>
+                <span className="ml-2">{clickedStatus[index] ? 'Copied!' : 'COPY'}</span>
               </button>
               <div className="pb-2 mb-2 text-md text-amber-50">{feature.description}</div>
             </div>
